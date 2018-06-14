0178a5d00d80049e3aca6b17a63b4f87d0282a2e
Aaron DeLong <aarondelong93@gmail.com>
2018-06-14 13:37:47 -0400

    Working Android App

--- app/src/main/java/com/example/aarondelong/uielementsexamples/MainActivity.java
+++ app/src/main/java/com/example/aarondelong/uielementsexamples/MainActivity.java
@@ -2,12 +2,38 @@ package com.example.aarondelong.uielementsexamples;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private EditText editText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        textView.setText("This text is changed in the onCreate");
+
+         setListener();
+    }
+
+    private void setListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Take input from editText and set as value for textView
+                textView.setText(editText.getText().toString());
+            }
+        });
     }
 }
